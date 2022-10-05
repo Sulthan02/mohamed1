f070b234044135936e77c590879f3ecdea0ccb5b
Sulthan02 <sulthanaym01@gmail.com>
2022-10-05 01:07:45 -0400

    first program

--- First.java
+++ First.java
@@ -3,7 +3,7 @@ class First
 public static void main (String()args);
 {
 System.out.println("mohamed sulthan");
-System.out.println("hello java");
+System.out.println("hello java happy coding");
 }
 }
 
